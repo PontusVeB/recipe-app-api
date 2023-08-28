54eb724904f27e3f70e40569e8b4bb16bae18ef2
John Doe <Johndoe@example.com>
2023-08-28 16:18:47 +0200

    14 sections

---
+++ app/user/serializers.py
@@ -0,0 +1,57 @@
+"""
+Serializers for the user API VIEW
+"""
+from django.contrib.auth import (
+    get_user_model,
+    authenticate
+)
+from django.utils.translation import gettext as _
+
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializers for the user object"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email','password','name']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Creat and return a user with encrypter password"""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """update and return user."""
+        password = validated_data.pop('password', None) #zabiera hasło z requesta -> pop, password is optional None as default
+        user = super().update(instance, validated_data) #updating object
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style ={'input_type':'password'},
+        trim_whitespace = False,
+    )
+
+    def validate(self, attrs):
+        """validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username = email,
+            password=password,
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
